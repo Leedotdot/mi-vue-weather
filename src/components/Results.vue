de40a6ec6e213c775e1c3f1e22ee41a95df44a60
Kenta Tanaka <poru0812@gmail.com>
2022-07-30 08:39:02 +0900

    make vue-weather-app

---
+++ src/components/Results.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="results-country" v-if="country">
+        {{ country }}
+    </div>
+    <div class="results-city" v-if="cityName">
+        {{ cityName }}
+    </div>
+    <div class="results-temp" v-if="temperature">
+        {{ temperature }}<span>℃</span>
+    </div>
+    <div class="reults-condition" v-if="icon">
+        <img :src="icon" alt="icon">
+        <span>{{ conditionText }}</span>
+    </div>
+</template>
+
+<script setup>
+import { toRefs } from 'vue'
+const props = defineProps({
+    results: Object
+})
+
+const {country, cityName, temperature, icon, conditionText} = toRefs(props.results)
+</script>
